5bdc79ec36526ae71bde577edf25670fc54c803c
pi <eliaskoschke@gmail.com>
2025-02-13 13:03:49 +0100

    fixed

--- src/main/java/RasPiTest.java
+++ src/main/java/RasPiTest.java
@@ -2,32 +2,41 @@ import com.pi4j.Pi4J;
 import com.pi4j.io.gpio.digital.DigitalInput;
 import com.pi4j.io.gpio.digital.DigitalState;
 import com.pi4j.io.gpio.digital.PullResistance;
-import com.pi4j.util.Console;
 
 public class RasPiTest {
-    public static void main(String[] args) {
-        final  Console console = new Console();
-
+    public static void main(String[] args) throws InterruptedException {
         var pi4j = Pi4J.newAutoContext();
 
         var config = DigitalInput.newConfigBuilder(pi4j)
-                .id("Button")
-                .name("Button")
-                .address(2)
+                .id("Button1")
+                .name("Button Test1")
+                .address(27)
                 .pull(PullResistance.PULL_DOWN)
-                .build();
+                .debounce(1000L);
+
+        var config2 = DigitalInput.newConfigBuilder(pi4j)
+                .id("Button2")
+                .name("Button Test2")
+                .address(22)
+                .pull(PullResistance.PULL_DOWN)
+                .debounce(1000L);
 
         var button = pi4j.create(config);
+        var button2 = pi4j.create(config2);
 
         button.addListener(e -> {
-           if (e.state() == DigitalState.HIGH) {
-               console.println("Knopf gedrückt");
-           } else {
-               console.println("Knopf losgelassen");
-           }
+            if (e.state() == DigitalState.HIGH) {
+                System.out.println("Knopf 1 gedrückt");
+            }
+        });
+        button2.addListener(e -> {
+            if (e.state() == DigitalState.HIGH) {
+                System.out.println("Knopf 2 gedrückt");
+            }
         });
 
-        console.promptForExit();
+        Thread.sleep(60000);
+
         pi4j.shutdown();
     }
 }
