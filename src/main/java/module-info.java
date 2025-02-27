1ad47df7ba1ed859e254f5cf92156b85c28b57fd
Noel Levin Hirsch <noel.levin.hirsch@schwaebisch-hall.de>
2025-02-27 11:09:50 +0100

    Es klappt jetzt

---
+++ src/main/java/module-info.java
@@ -0,0 +1,20 @@
+module Der.Geraet.Maven {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires spring.boot.autoconfigure;
+    requires com.pi4j;
+    requires com.fasterxml.jackson.databind;
+    requires spring.boot;
+    requires spring.context;
+    requires org.apache.httpcomponents.httpclient;
+    requires org.apache.httpcomponents.httpcore;
+    requires org.bytedeco.javacv;
+    requires java.desktop;
+    requires com.google.zxing.javase;
+    requires com.google.zxing;
+    requires spring.beans;
+    requires spring.web;
+
+    opens com.example to javafx.graphics, spring.beans, spring.context;
+    exports com.example;
+}
