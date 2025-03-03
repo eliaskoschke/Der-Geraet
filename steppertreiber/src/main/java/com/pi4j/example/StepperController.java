9bafd061d3b514f77b30f4a50c9472839602898c
KarlHeinzKeck <maxkaufmann2004@gmail.com>
2025-03-03 12:10:39 +0100

    Fertigstelllung des StepperControllers

--- steppertreiber/src/main/java/com/pi4j/example/StepperController.java
+++ steppertreiber/src/main/java/com/pi4j/example/StepperController.java
@@ -1,22 +1,28 @@
 package com.pi4j.example;
 
 import com.pi4j.Pi4J;
+import com.pi4j.context.Context;
 import com.pi4j.example.tmc2209.TMCCommunicationException;
 import com.pi4j.example.tmc2209.TMCDeviceIsBusyException;
 import com.pi4j.example.tmc2209.Tmc2209;
 
-import java.util.Scanner;
-
-public class SepperController {
+public class StepperController {
     private static final int PIN_INDEX = 23;
     private static final int PIN_STEP = 18;
     private static final int PIN_DIR = 24;
-    private static final int SPEED = 2000;
+    private static final int SPEED = 500;
+    private static final int TRESHOLD = 35; //TODO: Rausfinden, wieviel ein Treshold haben sollte
     Tmc2209 steppermotor;
 
-    public SepperController() throws TMCCommunicationException, InterruptedException {
-        var pi4j = Pi4J.newAutoContext();
+    Context pi4j;
+
+    public StepperController(Context pi4j) throws TMCCommunicationException, InterruptedException {
+        this.pi4j = pi4j;
         steppermotor = new Tmc2209(pi4j, PIN_INDEX, PIN_DIR, PIN_STEP);
+    }
+
+    public void orientieren() throws TMCDeviceIsBusyException {
+        steppermotor.homePosition(SPEED,TRESHOLD);
     }
 
     void turn(int grad) throws TMCDeviceIsBusyException {
@@ -24,17 +30,7 @@ public class SepperController {
     }
 
     private int calc(int grad) {
-        int result = (int) (grad /1.8 * 2.6 * 8);
+        int result = (int) ((grad * 8) /(1.8 / 2.6 ));
         return result;
-    }
-
-    public static void main(String[] args) throws TMCCommunicationException, InterruptedException, TMCDeviceIsBusyException {
-        Scanner x = new Scanner(System.in);
-
-        SepperController motor = new SepperController();
-
-        while (true) {
-            motor.turn(x.nextInt());
-        }
     }
 }
