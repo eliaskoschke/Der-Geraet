47eacd0a415ee182ba1e1f769a0792dd6a93e7b3
marlongruen <marlon.a.gruen@gmail.com>
2025-03-18 12:00:34 +0100

    css play und startseite

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -13,11 +13,17 @@ window.onload = function isGeraetInPlace() {
     })
     .then(data => {
         if (data.message === "false") {
+            console.log('Der Gerät befindet sich im freien Modus');
             document.getElementById('admin').removeAttribute('disabled');
-            document.getElementById('deactiveText').classList.add('hidden');
+            const statusText = document.getElementById('deactiveText');
+            statusText.classList.add('hidden');
+            statusText.textContent = 'Der Gerät befindet sich im freien Modus';
         } else {
+            console.log('Der Gerät befindet sich im Tisch Modus');
             document.getElementById('admin').setAttribute('disabled', 'true');
-            document.getElementById('deactiveText').classList.remove('hidden');
+            const statusText = document.getElementById('deactiveText');
+            statusText.classList.remove('hidden');
+            statusText.textContent = 'Derzeit nicht möglich da der Gerät sich im Tisch befindet.';
         }
     })
     .catch(error => console.error('Fehler:', error));
